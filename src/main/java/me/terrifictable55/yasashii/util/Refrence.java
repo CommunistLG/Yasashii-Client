13ccf041bbb49545318fe230d35f6503ae6b5622
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2021-09-01 19:49:53 +0200

    Update Refrence.java

--- src/main/java/me/terrifictable55/yasashii/util/Refrence.java
+++ src/main/java/me/terrifictable55/yasashii/util/Refrence.java
@@ -5,7 +5,7 @@ public class Refrence {
     /** just to only need to change one thing here and its changed in the intire client :D */
     public static final String MOD_ID = "yc";
     public static final String NAME = "Yasashii";
-    public static final String VERSION = "0.0.4"; /** Rewrite */
+    public static final String VERSION = "0.0.5"; /** Rewrite */
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
     public static final String CLIENT_PROXY_CLASS = "me.terrifictable55.yasashii.proxy.ClientProxy";
     public static final String COMMON_PROXY_CLASS = "me.terrifictable55.yasashii.proxy.CommonProxy";
